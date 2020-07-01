faa1b49e55172558af6769f6ed3cb678465d904a
SJLEE316 <lsj8249@naver.com>
2020-07-01 20:45:56 +0900

    navbar

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 import introduction.urls
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('introduction/', include('introduction.urls')),
+    path('', views.main, name="main"),
 ]
