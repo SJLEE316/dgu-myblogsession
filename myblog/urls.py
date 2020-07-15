318095ad40885d30a105d497a44f9d26d31550ed
SJLEE316 <lsj8249@naver.com>
2020-07-15 20:24:09 +0900

    Upadate and delete and media

--- myblog/urls.py
+++ myblog/urls.py
@@ -17,9 +17,15 @@ from django.contrib import admin
 from django.urls import path,include
 import introduction.urls
 from . import views
+from django.conf import settings 
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('introduction/', include('introduction.urls')),
     path('', views.main, name="main"),
+    path('posts/', include('posts.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
