39cfb9b7c7ebcf95c3d10a82025d446a1a914dc4
SJLEE316 <lsj8249@naver.com>
2020-09-02 20:22:05 +0900

    like

--- posts/models.py
+++ posts/models.py
@@ -10,10 +10,26 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now = True)
     image = models.ImageField(upload_to='images/', null=True)
     user = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
+    like_user_set = models.ManyToManyField(User, blank=True, related_name='like_user_set', through='Like') 
+    #좋아요가 없어도 문제ㄴㄴ-> True, through는 중간에 Like가 있다. (중간다리)
 
-class Comment(models.Model):
+    @property #좋아요 몇 개?
+    def like_count(self):
+        return self.like_user_set.count()
+
+
+class Comment(models.Model): #1대N관계
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now = True)
     writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name='comments')
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (('user','post'))  #unique한 관계. 중복되는 것이 없다
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post, Comment
+from .models import *
+from django.contrib.auth.decorators import login_required
 
 def new(request):
     return render(request, 'posts/new.html')
@@ -11,11 +12,11 @@ def create(request):
         image=request.FILES.get('image')
         user = request.user
         Post.objects.create(title=title, content=content, image=image, user=user)
-    return redirect('posts:main')    
+    return redirect('posts:main')
 
 def main(request):
     posts = Post.objects.all()
-    return render(request, 'posts/main.html', {'posts': posts})   
+    return render(request, 'posts/main.html', {'posts': posts})
 
 def show(request, id):
     post = Post.objects.get(pk=id)
@@ -37,7 +38,7 @@ def update(request, id):
 def delete(request, id):
     post = get_object_or_404(Post, pk=id)
     post.delete()
-    return redirect('posts:main')    
+    return redirect('posts:main')
 
 def create_comment(request, post_id) : #어느 게시글?
     if request.method == "POST":
@@ -45,9 +46,29 @@ def create_comment(request, post_id) : #어느 게시글?
         current_user = request.user
         comment_content = request.POST.get('content') #textarea
         Comment.objects.create(content=comment_content, writer=current_user, post=post) #모델=우리가 넣어줄 값
-    return redirect('posts:show', post.pk)    #POST가 아니어도 return가능
+    return redirect('posts:show', post.pk) #POST가 아니어도 return가능
 
+@login_required
+def post_like(request, post_id): #어떤 포스트?
+    post = get_object_or_404(Post, pk=post_id)
+    post_like, post_like_created = post.like_set.get_or_create(user=request.user)
 
+    # if request.user in post.like_user_set.all(): #user가 안에 있으면
+    #     post.like_user_set.remove(request.user)
+    # else:
+    #     post.like_user_set.add(request.user)
 
+    if not post_like_created:
+        post_like.delete()
+
+    if request.GET.get('redirect_to') == 'show':
+        return redirect('posts:show', post_id)
+    else:
+        return redirect('posts:main')
+
+@login_required
+def like_list(request):
+    likes = request.user.like_set.all()
+    return render(request,'posts/like_list.html',{'likes':likes})
 
 # Create your views here.
